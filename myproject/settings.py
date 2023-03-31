210268db030e8b6de12c85758e0d95a10009c0ec
Allen <f0934015254@gmail.com>
2023-03-31 17:46:29 +0800

    My First Docker Practice

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'myblog',                      
+        'USER': 'root',                      
+        'PASSWORD': '0325',               
+        'HOST': 'db',                           
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},                           
     }
 }
 
